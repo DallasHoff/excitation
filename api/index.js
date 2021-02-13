688edace14a2d3a32e71d48c9cecf64def168d6e
Dallas Hoffman <dallashoff@gmail.com>
2021-02-12 20:53:46 -0500

    API 404 handler

--- api/index.js
+++ api/index.js
@@ -2,12 +2,17 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-
-app.get('/', (req, res) => {
-    res.send('Hello from Excitation!');
+// Test endpoint
+app.get('/test', (req, res) => {
+    res.json({message: 'Hello from Excitation!'});
 });
 
+// Handle 404's
+app.use((req, res, next) => {
+    res.status(404).json({error: '404: Endpoint Not Found'});
+});
 
+// Listen for requests
 app.listen(port, () => {
     console.log(`Express app listening at http://localhost:${port}`);
 });
