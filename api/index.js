5319a888ee5e9f3a53cce4bc4fbd6b285348513e
Dallas Hoffman <dallashoff@gmail.com>
2021-02-15 00:11:10 -0500

    Initial work on web page endpoint

--- api/index.js
+++ api/index.js
@@ -1,15 +1,36 @@
 const express = require('express');
+const axios = require('axios');
+const cheerio = require('cheerio');
 const app = express();
 const port = 3000;
 
 // Test endpoint
 app.get('/test', (req, res) => {
-    res.json({message: 'Hello from Excitation!'});
+    return res.json({message: 'Hello from Excitation!'});
+});
+
+// Cite web page
+app.get('/cite/webpage', async (req, res) => {
+    var result = {};
+    // URL to cite
+    var q = req.query.q;
+    if (!q) return res.status(400).json({error: 'No URL was provided.'});
+    // Get web page HTML
+    try {
+        var {data: html} = await axios.get(q);
+        var $ = cheerio.load(html);
+    } catch (err) {
+        return res.status(400).json({error: 'The URL provided does not lead to a valid web page.'});
+    }
+    // Extract citation info (TODO)
+    result.title = $('head title').first().text().trim();
+    // Respond
+    return res.status(200).json(result);
 });
 
 // Handle 404's
 app.use((req, res, next) => {
-    res.status(404).json({error: '404: Endpoint Not Found'});
+    return res.status(404).json({error: 'Endpoint Not Found'});
 });
 
 // Listen for requests
